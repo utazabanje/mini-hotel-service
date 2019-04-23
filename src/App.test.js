cb48d2fc955602e1e422ca413dbd5f44a6d2de58
Miodrag Piletic <utazabanje@hotmail.com>
2019-04-23 08:34:03 +0200

    setup project with sample test

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({adapter: new Adapter()});
+
+describe('App js onAdd function', () => {
+  it('should return 4', () => {
+    const appComp = shallow(<App/>);
+    expect(appComp.instance().onAdd(1,3)).toBe(4);
+  });
+});
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
